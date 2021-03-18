4390c97bbb09f4bad2d6e57d9052f26d5b33275a
aponyavina <ponyavina1997@mail.ru>
2021-03-18 17:51:24 +0300

    creating navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="wrapper">
 
+    <header>
+      <div class="navbar">
+        <div class="container">
+          <div class="navbar-content">
+            <div class="logo">VUE-CLI</div>
+            <ul class="navbar-list">
+              <li class="navbar-item">
+                <a href="" class="navbar-link">Home</a>
+              </li>
+              <li class="navbar-item">
+                <a href="" class="navbar-link">Example</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+
     <router-view></router-view>
   </div>
 </template>
